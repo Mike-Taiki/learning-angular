4b0b77375c856dac4d1b315a348e4c2d2d32b18f
Michael Gomes <michael.21.2012@hotmail.com>
2019-11-27 18:50:05 -0300

    viewed ngStyle and ngClass

--- 04/directives-app/src/app/app.module.ts
+++ 04/directives-app/src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgStyleComponent } from "./ng-style/ng-style.component";
+import { FormsModule } from "@angular/forms";
+import {
+  MatCardModule,
+  MatRadioModule,
+  MatSelectModule
+} from "@angular/material";
+import { NgClassComponent } from './ng-class/ng-class.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NgStyleComponent, NgClassComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCardModule,
+    MatRadioModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
