4894a2303a4a33b2fe29bc3cf5526af429628bc5
Michael Gomes <michael.21.2012@hotmail.com>
2019-11-28 20:15:16 -0300

    eventEmitter

--- 05/communication-app/src/app/app.module.ts
+++ 05/communication-app/src/app/app.module.ts
@@ -1,22 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InputBindingComponent } from './input-binding/input-binding.component';
-import { ClientComponent } from './input-binding/client/client.component';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { InputBindingComponent } from "./input-binding/input-binding.component";
+import { ClientComponent } from "./input-binding/client/client.component";
+import { EventComponent } from "./event/event.component";
+
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatIconModule
+} from "@angular/material";
+import { ChildItemComponent } from "./event/child-item/child-item.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     InputBindingComponent,
-    ClientComponent
+    ClientComponent,
+    EventComponent,
+    ChildItemComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
