2bf5ab7ef91df54bd45a41b643d9dda3930b65f8
Michael Gomes <michael.21.2012@hotmail.com>
2019-12-02 23:05:37 -0300

    edit

--- 06/lifecycle/src/app/main-lifecycle/main-lifecycle.component.ts
+++ 06/lifecycle/src/app/main-lifecycle/main-lifecycle.component.ts
@@ -12,20 +12,36 @@ export class MainLifecycleComponent implements OnInit {
   private name: string;
   private age: number;
   private food: string;
-  private editClient: Client = null;
+  private editClient: number = -1;
 
   constructor() {}
 
   ngOnInit() {}
 
   save() {
-    if (this.editClient === null) {
+    if (this.editClient === -1) {
       this.clients.push({
         name: this.name,
         age: this.age,
         food: this.food
       });
-      (this.name = ""), (this.age = null), (this.food = "");
+    } else {
+      this.clients[this.editClient].name = this.name;
+      this.clients[this.editClient].age = this.age;
+      this.clients[this.editClient].food = this.food;
+      this.editClient = -1;
     }
+    (this.name = ""), (this.age = null), (this.food = "");
+  }
+
+  remove(i: number) {
+    this.clients.splice(i, 1);
+  }
+
+  edit(i: number) {
+    this.name = this.clients[i].name;
+    this.age = this.clients[i].age;
+    this.food = this.clients[i].food;
+    this.editClient = i;
   }
 }
