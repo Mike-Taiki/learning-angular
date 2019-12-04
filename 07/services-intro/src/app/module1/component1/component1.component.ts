01a821fd9f2389f96865f6dfed5cd30240d8ea04
Michael <michael.21.2012@hotmail.com>
2019-12-04 12:03:46 -0300

    learning services

---
+++ 07/services-intro/src/app/module1/component1/component1.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Service1 } from '../service1.service';
+
+@Component({
+  selector: 'app-component1',
+  templateUrl: './component1.component.html',
+  styleUrls: ['./component1.component.css']
+})
+export class Component1Component implements OnInit {
+  
+  num = 0;
+
+  constructor(private myService1: Service1) { }
+
+  ngOnInit() {
+  }
+
+}
