b12f58d083818047675972cfd5eb9164275c8ce5
Michael Gomes <michael.21.2012@hotmail.com>
2019-12-09 00:04:07 -0300

    using reducers

--- 24/ngrx-basic/src/app/store/person.reducer.ts
+++ 24/ngrx-basic/src/app/store/person.reducer.ts
@@ -11,11 +11,16 @@ export function reducer(
     case fromPersonActions.PersonActionTypes.PERSON_ALL:
       return state;
     case fromPersonActions.PersonActionTypes.PERSON_DELETE:
-      return state;
+      return state.filter(p => p._id !== action.payload.id);
     case fromPersonActions.PersonActionTypes.PERSON_NEW:
-      return state;
+      return state.concat([action.payload.person]);
     case fromPersonActions.PersonActionTypes.PERSON_UPDATE:
-      return state;
+      const people = Array.from(state);
+      const i = people.findIndex(p => p._id === action.payload.person._id);
+      if (i >= 0) {
+        people[i] = action.payload.person;
+      }
+      return people;
     default:
       return state;
   }
